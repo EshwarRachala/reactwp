d7c2cba8c246ac6f55e1f91f5bc5380df8758f0e
eshwarrachala <rachalaeshwar1@gmail.com>
2017-02-19 00:11:39 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
+const style = {
+  margin: 12,
+};
+
 class App extends Component {
   render() {
     return (<div>
-      <h1>Hi from react Component hello</h1>
-      <RaisedButton label="Default" />
+      <h1>Hi from react Component</h1>
+      <RaisedButton label="Submit" secondary={true} style={style} />
     </div>);
   }
 }
