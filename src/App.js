dea8cff9afca19375710ccf274ffc8f3c7982fa9
eshwarrachala <rachalaeshwar1@gmail.com>
2017-02-18 23:45:05 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/*eslint-disable */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (<h1>Hi from react and webpack 2 demo </h1>);
+  }
+}
+export default App;
---
+++ test/unit/specs/Hello.spec.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import Hello from 'src/components/Hello';
+
+// describe('Hello.vue', () => {
+//   it('should render correct contents', () => {
+//     const Constructor = Vue.extend(Hello);
+//     const vm = new Constructor().$mount();
+//     expect(vm.$el.querySelector('.hello h1').textContent)
+//       .to.equal('Welcome to Your Vue.js App');
+//   });
+// });
