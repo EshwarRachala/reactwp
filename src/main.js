420e5aeca46cb8a361fbb82f553ba57cc4a9e177
eshwarrachala <rachalaeshwar1@gmail.com>
2017-02-19 22:15:13 -0500

    replaced daskbase with light theme

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { cyan500 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -15,14 +16,30 @@ import AppBar from 'material-ui/AppBar';
 
 import App from './App';
 
+const muiTheme = getMuiTheme({
+    palette: {
+        textColor: cyan500
+    },
+    appBar: {
+        height: 50
+    }
+});
+
+const styles = {
+    title: {
+        color: 'white'
+    }
+};
+
 render(
   <Provider store={store}>
-    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-    <div>
-    <AppBar title="React/Webpack 2 Project Demo" />
-      <App />
-    </div>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <div>
+          <AppBar title={<span style={styles.title}>React Webpack</span>}/>
+          <App />
+      </div>
     </MuiThemeProvider>
-  </Provider>, document.getElementById('app')
+  </Provider>,
+  document.getElementById('app')
 );
 
