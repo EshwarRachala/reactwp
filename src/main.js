e44b615bff85e7e729dafb4ed6b66b5afb4affc7
eshwarrachala <rachalaeshwar1@gmail.com>
2017-02-19 22:27:33 -0500

    added components

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
 injectTapEventPlugin();
 
 import configureStore from './store';
@@ -27,7 +28,7 @@ const muiTheme = getMuiTheme({
 
 const styles = {
     title: {
-        color: 'white'
+        bold: 'true'
     }
 };
 
